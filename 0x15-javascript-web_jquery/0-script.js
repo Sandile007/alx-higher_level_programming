aef8e4e246d7b35ea985fa800bf08c4e1cc15ed7
Sandile Mbokazi <chynisemczee@gmail.com>
2024-06-03 04:08:54 -0700

    Changeing Header Color in Jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,10 @@
+// Get the <header> element using document.querySelector
+const headerElement = document.querySelector("#header");
+
+// Function to change the text color
+function changeTextColor() {
+headerElement.style.color = "#FF0000"; // Red color
+}
+
+// Call the function to change the color (you can trigger this based on user interaction)
+changeTextColor();
