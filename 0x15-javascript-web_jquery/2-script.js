010c9d17a626eca5104f6c4f84ec7a261d366e5c
Sandile Mbokazi <chynisemczee@gmail.com>
2024-06-03 04:25:00 -0700

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// When the user clicks on the #red_header div
+$("#red_header").on("click", function() {
+// Get the <header> element by its ID
+const headerElement = document.getElementById("header");
+
+// Change the text color to red (#FF0000)
+headerElement.style.color = "#FF0000";
+});
